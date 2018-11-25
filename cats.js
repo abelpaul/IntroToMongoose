27232078f48b0239227156c1f15548f33b1802d0
Abel George <abel.paul.george@gmail.com>
2018-11-25 18:08:23 -0500

    added comments

--- cats.js
+++ cats.js
@@ -1,13 +1,14 @@
 var mongoose = require("mongoose")
 mongoose.connect("mongodb://localhost/cat_app")
 
-
+// added schema for a cat object
 var catSchema = new mongoose.Schema({
     name: String,
     age: Number,
     temperament: String
 });
 
+// set the var cat to this schema we created to create new cat objects
 var Cat = mongoose.model("Cat", catSchema);
 
 // adding a new cat to the DB
@@ -37,3 +38,4 @@ Cat.find({},function(err,cats){
         console.log(cats)
     }
 })
+
